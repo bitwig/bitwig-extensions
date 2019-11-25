7d5a0bdf4815ef825a53411e4842d463dc2f86c3
Nicholas Allen <nick@bitwig.com>
2019-11-25 13:01:51 +0100

    Convert the generic script from Java script to Java

---
+++ src/main/java/com/bitwig/extensions/controllers/generic/MIDIKeyboardExtensionDefinition.java
@@ -0,0 +1,81 @@
+package com.bitwig.extensions.controllers.generic;
+import java.util.UUID;
+
+import com.bitwig.extension.api.PlatformType;
+import com.bitwig.extension.controller.AutoDetectionMidiPortNamesList;
+import com.bitwig.extension.controller.ControllerExtensionDefinition;
+import com.bitwig.extension.controller.api.ControllerHost;
+
+public class MIDIKeyboardExtensionDefinition extends ControllerExtensionDefinition
+{
+   private static final UUID DRIVER_ID = UUID.fromString("6E55D132-1846-4C64-9F97-48041F2D9B96");
+
+   public MIDIKeyboardExtensionDefinition()
+   {
+   }
+
+   @Override
+   public String getName()
+   {
+      return "MIDI Keyboard";
+   }
+
+   @Override
+   public String getAuthor()
+   {
+      return "Bitwig";
+   }
+
+   @Override
+   public String getVersion()
+   {
+      return "3.1";
+   }
+
+   @Override
+   public UUID getId()
+   {
+      return DRIVER_ID;
+   }
+
+   @Override
+   public String getHardwareVendor()
+   {
+      return "Generic";
+   }
+
+   @Override
+   public String getHardwareModel()
+   {
+      return "MIDI Keyboard";
+   }
+
+   @Override
+   public int getRequiredAPIVersion()
+   {
+      return 10;
+   }
+
+   @Override
+   public int getNumMidiInPorts()
+   {
+      return 1;
+   }
+
+   @Override
+   public int getNumMidiOutPorts()
+   {
+      return 0;
+   }
+
+   @Override
+   public void listAutoDetectionMidiPortNames(final AutoDetectionMidiPortNamesList list, final PlatformType platformType)
+   {
+   }
+
+   @Override
+   public MIDIKeyboardExtension createInstance(final ControllerHost host)
+   {
+      return new MIDIKeyboardExtension(this, host);
+   }
+}
