dca471bede2ae2ad969d23ef5cb53261a1f3006f
Alexandre BIQUE <bique.alexandre@gmail.com>
2019-11-25 15:28:33 +0100

    Fix compilation after refactoring

--- src/main/java/com/bitwig/extensions/controllers/arturia/keylab/mk2/ArturiaKeylabMkII.java
+++ src/main/java/com/bitwig/extensions/controllers/arturia/keylab/mk2/ArturiaKeylabMkII.java
@@ -259,10 +259,14 @@ public abstract class ArturiaKeylabMkII extends ControllerExtension
       mMultiLayer = new Layer(mLayers, "Multi")
       {
          @Override
-         public void setIsActive(final boolean active)
+         protected void onActivate()
          {
-            super.setIsActive(active);
+            updateIndications();
+         }
 
+         @Override
+         protected void onDeactivate()
+         {
             updateIndications();
          }
       };
