e6f686ae7df15485ea3d9f0d631f4eb7dcf56730
Alexandre BIQUE <bique.alexandre@gmail.com>
2019-11-27 11:08:35 +0100

    Rework the multiplexer

--- src/main/java/com/bitwig/extensions/controllers/novation/launchpad_pro/MultiplexerMode.java
+++ src/main/java/com/bitwig/extensions/controllers/novation/launchpad_pro/MultiplexerMode.java
@@ -1,13 +1,22 @@
 package com.bitwig.extensions.controllers.novation.launchpad_pro;
 
-public class MultiplexerMode extends Mode
+class MultiplexerMode extends Mode
 {
-   private static final Color ACTIVE_MODE_COLOR = Color.fromRgb255(255, 255, 255);
-   private static final Color INACTIVE_MODE_COLOR = Color.scale(ACTIVE_MODE_COLOR, 0.2f);
-
    MultiplexerMode(final LaunchpadProControllerExtension driver, final String name)
    {
       super(driver, name);
+
+      for (int i = 0; i < 8; ++i)
+      {
+         final int I = i;
+         final Button button = driver.getSceneButton(7 - i);
+         bindPressed(button, () -> selectMinorMode(I));
+         bindLightState(() -> {
+            if (mModes[I] == null)
+               return LedState.OFF;
+            return mSelectedIndex == I ? LedState.SHIFT_ON : LedState.SHIFT_OFF;
+         }, button);
+      }
    }
 
    @Override
@@ -41,104 +50,6 @@ public class MultiplexerMode extends Mode
       setMode(index, mode, null);
    }
 
-   @Override
-   public void paint()
-   {
-      if (mIsInPaint)
-         return;
-
-      mIsInPaint = true;
-      super.paint();
-      mModes[mSelectedIndex].paint();
-      mIsInPaint = false;
-
-      for (int i = 0; i < 8; ++i)
-      {
-         final Button button = mDriver.getButtonOnTheRight(7 - i);
-
-         if (mModes[i] == null)
-            button.clear();
-         else if (mSelectedIndex == i)
-            button.setColor(ACTIVE_MODE_COLOR);
-         else
-            button.setColor(INACTIVE_MODE_COLOR);
-      }
-   }
-
-   @Override
-   public void paintModeButton()
-   {
-      super.paintModeButton();
-
-      mModes[mSelectedIndex].paintModeButton();
-   }
-
-   @Override
-   public void onPadPressed(final int x, final int y, final int velocity)
-   {
-      mModes[mSelectedIndex].onPadPressed(x, y, velocity);
-   }
-
-   @Override
-   public void onPadReleased(final int x, final int y, final int velocity, final boolean wasHeld)
-   {
-      mModes[mSelectedIndex].onPadReleased(x, y, velocity, wasHeld);
-   }
-
-   @Override
-   public void onArrowUpReleased()
-   {
-      mModes[mSelectedIndex].onArrowUpReleased();
-   }
-
-   @Override
-   public void onArrowUpPressed()
-   {
-      mModes[mSelectedIndex].onArrowUpPressed();
-   }
-
-   @Override
-   public void onArrowDownReleased()
-   {
-      mModes[mSelectedIndex].onArrowDownReleased();
-   }
-
-   @Override
-   public void onArrowDownPressed()
-   {
-      mModes[mSelectedIndex].onArrowDownPressed();
-   }
-
-   @Override
-   public void onArrowRightPressed()
-   {
-      mModes[mSelectedIndex].onArrowRightPressed();
-   }
-
-   @Override
-   public void onArrowRightReleased()
-   {
-      mModes[mSelectedIndex].onArrowRightReleased();
-   }
-
-   @Override
-   public void onArrowLeftPressed()
-   {
-      mModes[mSelectedIndex].onArrowLeftPressed();
-   }
-
-   @Override
-   public void onArrowLeftReleased()
-   {
-      mModes[mSelectedIndex].onArrowLeftReleased();
-   }
-
-   @Override
-   public void onSceneButtonPressed(final int column)
-   {
-      selectMinorMode(7 - column);
-   }
-
    public void selectMinorMode(final int index)
    {
       if ((index < 0 && 7 < index) ||
@@ -165,7 +76,6 @@ public class MultiplexerMode extends Mode
       mModes[mSelectedIndex].updateKeyTranslationTable(table);
    }
 
-   private boolean mIsInPaint = false;
    private int mSelectedIndex = 0;
 
    private final Mode[] mModes = {null, null, null, null, null, null, null, null};
