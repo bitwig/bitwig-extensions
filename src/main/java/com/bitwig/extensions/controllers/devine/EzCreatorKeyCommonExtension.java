083a1aa7efdbec2420c814bfdfee987395e32a6f
unknown <niklas@bitwig.com>
2020-06-10 16:17:49 +0200

    Changed EZ-Creator Key (XL) implementation to use newer API style

--- src/main/java/com/bitwig/extensions/controllers/devine/EzCreatorKeyCommonExtension.java
+++ src/main/java/com/bitwig/extensions/controllers/devine/EzCreatorKeyCommonExtension.java
@@ -1,14 +1,19 @@
 package com.bitwig.extensions.controllers.devine;
 
 import com.bitwig.extension.controller.ControllerExtension;
+import com.bitwig.extension.controller.api.AbsoluteHardwareKnob;
 import com.bitwig.extension.controller.api.ControllerHost;
 import com.bitwig.extension.controller.api.CursorRemoteControlsPage;
 import com.bitwig.extension.controller.api.CursorTrack;
 import com.bitwig.extension.controller.api.HardwareControlType;
+import com.bitwig.extension.controller.api.HardwareSlider;
+import com.bitwig.extension.controller.api.HardwareSurface;
 import com.bitwig.extension.controller.api.MidiIn;
 import com.bitwig.extension.controller.api.MidiOut;
 import com.bitwig.extension.controller.api.PinnableCursorDevice;
 import com.bitwig.extension.controller.api.RemoteControl;
+import com.bitwig.extensions.framework.Layer;
+import com.bitwig.extensions.framework.Layers;
 
 public class EzCreatorKeyCommonExtension extends ControllerExtension
 {
@@ -25,44 +30,54 @@ public class EzCreatorKeyCommonExtension extends ControllerExtension
    public void init()
    {
       final String modelName     = getExtensionDefinition().getName();
-
+      final String    modelNameL= modelName.toLowerCase().replace(" ","-");
       final ControllerHost host  = getHost();
       final MidiIn midiIn        = host.getMidiInPort(0);
 
+      // Note input
       midiIn.createNoteInput(modelName, "80????", "90????").setShouldConsumeEvents(true);
-      midiIn.setMidiCallback(this::onMidiIn);
 
-      mMidiOut = host.getMidiOutPort(0);
-      mMidiOut.sendSysex(EzCreatorCommon.INIT_SYSEX);
+      // set device to "Bitwig-Mode"
+      host.getMidiOutPort(0).sendSysex(EzCreatorCommon.INIT_SYSEX);
 
-      CursorTrack cursorTrack = host.createCursorTrack(0, 0);
-      PinnableCursorDevice cursorDevice = cursorTrack.createCursorDevice();
-      CursorRemoteControlsPage remoteControls = cursorDevice.createCursorRemoteControlsPage(1);
-      remoteControls.setHardwareLayout(HardwareControlType.KNOB, 1);
-      mParameter = remoteControls.getParameter(0);
+      CursorTrack              cursorTrack     = host.createCursorTrack(modelNameL, modelName, 0, 0, true);
+      PinnableCursorDevice     cursorDevice    = cursorTrack.createCursorDevice();
+                               mRemoteControls = cursorDevice.createCursorRemoteControlsPage(modelName, 1, "");
+
+      mParameter = mRemoteControls.getParameter(0);
       mParameter.markInterested();
       mParameter.setIndication(true);
-   }
 
-   private void onMidiIn(final int status, final int data1, final int data2)
-   {
-      if (status == 0xB0 && data1 == mMainSliderCC)
-      {
-         mParameter.set(data2, 128);
-      }
+      mHardwareSurface = getHost().createHardwareSurface();
+      mHardwareSurface.setPhysicalSize(40, 50);
+
+      mSlider = mHardwareSurface.createHardwareSlider("Slider");
+      mSlider.setAdjustValueMatcher(midiIn.createAbsoluteCCValueMatcher(0, mMainSliderCC));
+      mHardwareSurface.hardwareElementWithId("Slider").setBounds(15, 10, 10, 30);
+
+      mLayers = new Layers(this);
+      mMainLayer = new Layer(mLayers, "Main");
+      mMainLayer.bind(mSlider, mRemoteControls.getParameter(0));
+      mMainLayer.activate();
    }
 
    @Override
    public void exit()
    {
-      mMidiOut.sendSysex(EzCreatorCommon.DEINIT_SYSEX);
+      getHost().getMidiOutPort(0).sendSysex(EzCreatorCommon.DEINIT_SYSEX);
    }
 
    @Override
    public void flush()
    { }
 
-   private int             mMainSliderCC;
-   private MidiOut         mMidiOut;
-   private RemoteControl   mParameter;
+   private int mMainSliderCC;
+
+   private CursorRemoteControlsPage mRemoteControls;
+
+   private HardwareSlider           mSlider;
+   private HardwareSurface          mHardwareSurface;
+   private RemoteControl            mParameter;
+   private Layers                   mLayers;
+   private Layer                    mMainLayer;
 }
