f3ffe4022909c8053b01dd082c5b48dbfb61f3d0
ericahrens <eric.n.ahrens@googlemail.com>
2021-06-21 16:48:08 +0200

    added ring display target

---
+++ src/main/java/com/bitwig/extensions/controllers/mackie/targets/RingDisplay.java
@@ -0,0 +1,32 @@
+package com.bitwig.extensions.controllers.mackie.targets;
+
+import com.bitwig.extension.controller.api.MidiOut;
+import com.bitwig.extensions.controllers.mackie.Midi;
+
+public class RingDisplay {
+	private final MidiOut midi;
+	private final int index;
+	private int lastValue = -1;
+
+	public RingDisplay(final MidiOut midi, final int index) {
+		this.index = index;
+		this.midi = midi;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void sendValue(final int value, final boolean showDot) {
+		final int newValue = value | (showDot ? 0x40 : 0x00);
+		if (newValue != lastValue) {
+			midi.sendMidi(Midi.CC, 0x30 | index, newValue);
+			lastValue = value;
+		}
+	}
+
+	public void refresh() {
+		midi.sendMidi(Midi.CC, 0x30 | index, lastValue);
+	}
+
+}
