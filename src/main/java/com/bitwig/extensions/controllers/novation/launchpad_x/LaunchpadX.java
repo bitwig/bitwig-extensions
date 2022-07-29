20cc6fcca99f688a52a76bd44d4eadb0881f6563
Elias <elias@bitwig.com>
2022-07-29 11:03:55 +0200

    improved Navigation of Launchpad X

--- src/main/java/com/bitwig/extensions/controllers/novation/launchpad_x/LaunchpadX.java
+++ src/main/java/com/bitwig/extensions/controllers/novation/launchpad_x/LaunchpadX.java
@@ -6,6 +6,7 @@ import java.util.function.Consumer;
 import com.bitwig.extension.api.Color;
 import com.bitwig.extension.controller.ControllerExtension;
 import com.bitwig.extension.controller.ControllerExtensionDefinition;
+import com.bitwig.extension.controller.api.Action;
 import com.bitwig.extension.controller.api.Application;
 import com.bitwig.extension.controller.api.Clip;
 import com.bitwig.extension.controller.api.ClipLauncherSlot;
@@ -37,6 +38,8 @@ import com.bitwig.extensions.framework.Layers;
 
 public class LaunchpadX extends ControllerExtension {
 
+    private static final int NOTES_MIDI_CHANNEL = 0x9f;
+    private static final int DRUM_MIDI_CHANNEL = 0x98;
     // SYSEX
     private final String DAW_MODE = "F0 00 20 29 02 0C 10 01 F7";
     private final String STANDALONE_MODE = "F0 00 20 29 02 0C 10 00 F7";
@@ -88,6 +91,38 @@ public class LaunchpadX extends ControllerExtension {
         mTransport.isClipLauncherOverdubEnabled().markInterested();
         mTransport.isMetronomeEnabled().markInterested();
         mTransport.isClipLauncherOverdubEnabled().markInterested();
+        
+        initCursorElements();
+        
+        mTrackBank = mHost.createTrackBank(8, 8, 8);
+        mTrackBank.followCursorTrack(mCursorTrack);
+        mTrackBank.cursorIndex().markInterested();
+        mTrackBank.canScrollBackwards().markInterested();
+        mTrackBank.canScrollForwards().markInterested();
+
+        initHardwareControlls();
+        initLayers();
+        initPadMatrix();
+        initScenes();
+        for (int i = 0; i < 4; i++) {
+            initFaderLayer(mMixerLayers[i], i, i * 8);
+        }
+        initButtonLayer(mMixerLayers[4], 0, RGBState.RED, RGBState.DARKRED);
+        initButtonLayer(mMixerLayers[5], 1, RGBState.ORANGE, RGBState.DARKORANGE);
+        initButtonLayer(mMixerLayers[6], 2, RGBState.YELLOW, RGBState.DARKYELLOW);
+        initButtonLayer(mMixerLayers[7], 3, RGBState.RED, RGBState.DARKRED);
+
+        initNavigation();
+        initTransport();
+
+        initHardwareLayout();
+
+        mSessionLayer.activate();
+
+        mHost.showPopupNotification("Launchpad Pro Mk3 initialized...");
+    }
+
+    private void initCursorElements() {
         mCursorTrack = mHost.createCursorTrack(8, 8);
         mCursorClip = mHost.createLauncherCursorClip(192, 128);
         mCursorClip.getLoopLength().markInterested();
@@ -119,32 +154,6 @@ public class LaunchpadX extends ControllerExtension {
             mCursorRemoteControlsPage.getParameter(i).exists().markInterested();
             mDeviceBank.getDevice(i).exists().markInterested();
         }
-        mTrackBank = mHost.createTrackBank(8, 8, 8);
-        mTrackBank.followCursorTrack(mCursorTrack);
-        mTrackBank.cursorIndex().markInterested();
-        mTrackBank.canScrollBackwards().markInterested();
-        mTrackBank.canScrollForwards().markInterested();
-
-        initHardwareControlls();
-        initLayers();
-        initPadMatrix();
-        initScenes();
-        for (int i = 0; i < 4; i++) {
-            initFaderLayer(mMixerLayers[i], i, i * 8);
-        }
-        initButtonLayer(mMixerLayers[4], 0, RGBState.RED, RGBState.DARKRED);
-        initButtonLayer(mMixerLayers[5], 1, RGBState.ORANGE, RGBState.DARKORANGE);
-        initButtonLayer(mMixerLayers[6], 2, RGBState.YELLOW, RGBState.DARKYELLOW);
-        initButtonLayer(mMixerLayers[7], 3, RGBState.RED, RGBState.DARKRED);
-
-        initNavigation();
-        initTransport();
-
-        initHardwareLayout();
-
-        mSessionLayer.activate();
-
-        mHost.showPopupNotification("Launchpad Pro Mk3 initialized...");
     }
 
     private void resetLastLayer() {
@@ -154,36 +163,14 @@ public class LaunchpadX extends ControllerExtension {
     private void midiCallback(String s) {
         mHost.println(s);
         if (isNoteIntputActive) {
-            for (int i = 0; i < 88; i++) {
-                mMidiOut.sendMidi(0x8f, i, 21);
-                if (mPlayingNotes != null && mPlayingNotes.length != 0) {
-                    mHost.println("PLayingNotes");
-
-                    for (PlayingNote n : mPlayingNotes) {
-                        if (n.pitch() == i)
-                            mMidiOut.sendMidi(0x9f, n.pitch(), 21);
-                    }
-                }
-            }
-
             if (mDrumPadBank.exists().get()) {
                 mMidiOut.sendSysex(DRUM_MODE);
-                for (int i = 0; i < 64; i++) {
-                    if (mDrumPadBank.getItemAt(i).exists().get()) {
-                        mMidiOut.sendMidi(0x98, 36 + i,
-                                new RGBState(mDrumPadBank.getItemAt(i).color().get()).getMessage());
-                        if (mPlayingNotes.length != 0) {
-                            for (PlayingNote n : mPlayingNotes) {
-                                if (n.pitch() == 36 + i)
-                                    mMidiOut.sendMidi(0x98, 36 + i, RGBState.WHITE.getMessage());
-                            }
-                        }
-                    } else
-                        mMidiOut.sendMidi(0x98, 36 + i, RGBState.OFF.getMessage());
-                }
+                sendPlayingNotesToDevice(true, DRUM_MIDI_CHANNEL, RGBState.WHITE);
 
-            } else
+            } else {
                 mMidiOut.sendSysex(NOTE_MODE);
+                sendPlayingNotesToDevice(false, NOTES_MIDI_CHANNEL, RGBState.GREEN);
+            }
         }
 
         if (s.startsWith(SESSION_MODE_PREFIX) && (mLastLayer == null || mLastLayer == mSessionLayer)) {
@@ -195,39 +182,63 @@ public class LaunchpadX extends ControllerExtension {
         }
 
         if (mMixerLayer.isActive()) {
-            for (int i = 0; i < 32; i++) {
-                final Track track = mTrackBank.getItemAt(i % 8);
-                if (i < 16)
-                    mMidiOut.sendMidi(0xb5, i,
-                            track.exists().get() ? new RGBState(track.color().get()).getMessage() : 0);
-                else if (i < 24) {
-                    RGBState r = new RGBState(track.sendBank().getItemAt(0).sendChannelColor().get());
-                    r = r.getMessage() == 0 ? RGBState.WHITE : r;
-                    mMidiOut.sendMidi(0xb5, i,
-                            track.exists().get() && track.trackType().get() != "Master" ? r.getMessage() : 0);
-                } else {
-                    RGBState r = new RGBState(track.sendBank().getItemAt(1).sendChannelColor().get());
-                    r = r.getMessage() == 0 ? RGBState.WHITE : r;
-                    mMidiOut.sendMidi(0xb5, i,
-                            track.exists().get() && track.trackType().get() != "Master" ? r.getMessage() : 0);
+            sendFaderValuesToDevice();
+            sendTrackControlsToDevice();
+        }
+    }
 
-                }
+    private void sendTrackControlsToDevice() {
+        for (int i = 0; i < 8; i++) {
+            final Track track = mTrackBank.getItemAt(i);
+
+            final Parameter volume = track.volume();
+            mMidiOut.sendMidi(0xb4, i, (int) (volume.get() * 127));
+
+            final Parameter pan = track.pan();
+            mMidiOut.sendMidi(0xb4, i + 8, (int) (pan.get() * 127));
+
+            final Parameter sendA = track.sendBank().getItemAt(0);
+            mMidiOut.sendMidi(0xb4, i + 16, (int) (sendA.get() * 127));
+
+            final Parameter sendB = track.sendBank().getItemAt(1);
+            mMidiOut.sendMidi(0xb4, i + 24, (int) (sendB.get() * 127));
+        }
+    }
+
+    private void sendFaderValuesToDevice() {
+        for (int i = 0; i < 32; i++) {
+            final Track track = mTrackBank.getItemAt(i % 8);
+            if (i < 16)
+                mMidiOut.sendMidi(0xb5, i,
+                        track.exists().get() ? new RGBState(track.color().get()).getMessage() : 0);
+            else if (i < 24) {
+                sendSendsValuesToDevice(i, track, 0);
+            } else {
+                sendSendsValuesToDevice(i, track, 1);
             }
-            for (int i = 0; i < 8; i++) {
-                final Track track = mTrackBank.getItemAt(i);
+        }
+    }
 
-                final Parameter volume = track.volume();
-                mMidiOut.sendMidi(0xb4, i, (int) (volume.get() * 127));
+    private void sendSendsValuesToDevice(int i, final Track track, int sendIndex) {
+        RGBState r = new RGBState(track.sendBank().getItemAt(1).sendChannelColor().get());
+        r = r.getMessage() == 0 ? RGBState.WHITE : r;
+        mMidiOut.sendMidi(0xb5, i,
+                track.exists().get() && track.trackType().get() != "Master" ? r.getMessage() : 0);
+    }
 
-                final Parameter pan = track.pan();
-                mMidiOut.sendMidi(0xb4, i + 8, (int) (pan.get() * 127));
-
-                final Parameter sendA = track.sendBank().getItemAt(0);
-                mMidiOut.sendMidi(0xb4, i + 16, (int) (sendA.get() * 127));
-
-                final Parameter sendB = track.sendBank().getItemAt(1);
-                mMidiOut.sendMidi(0xb4, i + 24, (int) (sendB.get() * 127));
+    private void sendPlayingNotesToDevice(boolean drums, int midi, RGBState onColor) {
+        if (drums) {
+            for (int i = 0; i < 64; i++) {
+                if (mDrumPadBank.getItemAt(i).exists().get()) 
+                    mMidiOut.sendMidi(0x98, 36 + i, new RGBState(mDrumPadBank.getItemAt(i).color().get()).getMessage());
             }
+        } else {
+            for (int j = 0; j < 88; j++) 
+                mMidiOut.sendMidi(0x8f, j, onColor.getMessage());
+        } 
+        if (mPlayingNotes != null && mPlayingNotes.length != 0) {
+            for (PlayingNote n : mPlayingNotes) 
+                mMidiOut.sendMidi(midi, n.pitch(), onColor.getMessage());
         }
     }
 
@@ -271,16 +282,19 @@ public class LaunchpadX extends ControllerExtension {
         mRightButton.setBackgroundLight(mRightLight);
         // SCENE BUTTONS
         for (int i = 0; i < 8; i++) {
-            mRightButtons[i] = createCCButton("scene" + (7 - i), (i + 1) * 10 + 9);
-            mRightLights[i] = createLight("scene_led" + (7 - i), (i + 1) * 10 + 9);
+            int midi = (i + 1) * 10 + 9;
+            String name = "scene" + (7 - i);
+            mRightButtons[i] = createCCButton(name, midi);
+            mRightLights[i] = createLight("led_" + name, midi);
             mRightButtons[i].setBackgroundLight(mRightLights[i]);
         }
         // BUTTON MATRIX
-
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                mButtons[i][j] = createNoteButton("" + (i * 10 + (7 - j)), (1 + i) + (j + 1) * 10);
-                mPadLights[i][j] = createLight("led" + (i * 10 + (7 - j)), (1 + i) + (j + 1) * 10);
+                int midi = (1 + i) + (j + 1) * 10;
+                String name = "" + (i * 10 + (7 - j));
+                mButtons[i][j] = createNoteButton(name, midi);
+                mPadLights[i][j] = createLight("led_" + name, midi);
                 mButtons[i][j].setBackgroundLight(mPadLights[i][j]);
             }
         }
@@ -332,10 +346,14 @@ public class LaunchpadX extends ControllerExtension {
 
         });
 
-        mSessionLayer.bindPressed(mUpButton, mSceneBank.scrollBackwardsAction());
-        mSessionLayer.bindPressed(mDownButton, mSceneBank.scrollForwardsAction());
-        mSessionLayer.bindPressed(mLeftButton, mTrackBank.scrollBackwardsAction());
-        mSessionLayer.bindPressed(mRightButton, mTrackBank.scrollForwardsAction());
+        //mSessionLayer.bindPressed(mUpButton, mSceneBank.scrollBackwardsAction());
+        //mSessionLayer.bindPressed(mDownButton, mSceneBank.scrollForwardsAction());
+        //mSessionLayer.bindPressed(mLeftButton, mTrackBank.scrollBackwardsAction());
+        //mSessionLayer.bindPressed(mRightButton, mTrackBank.scrollForwardsAction());
+        mSessionLayer.bindPressed(mRightButton, () -> pressedAction(mRightButton, () ->mTrackBank.scrollForwards()));
+        mSessionLayer.bindPressed(mLeftButton, () -> pressedAction(mLeftButton, () ->mTrackBank.scrollBackwards()));
+        mSessionLayer.bindPressed(mUpButton, () -> pressedAction(mUpButton, ()-> mSceneBank.scrollBackwards()));
+        mSessionLayer.bindPressed(mDownButton, () -> pressedAction(mDownButton, ()-> mSceneBank.scrollForwards()));
 
         mSessionLayer.bindLightState(
                 () -> mTrackBank.canScrollBackwards().get() ? RGBState.WHITE : RGBState.DARKGREY, mLeftLight);
@@ -347,6 +365,24 @@ public class LaunchpadX extends ControllerExtension {
                 () -> mSceneBank.canScrollForwards().get() ? RGBState.WHITE : RGBState.DARKGREY, mDownLight);
     }
 
+    private void pressedAction(HardwareButton button, Runnable action) {
+        action.run();
+        longPressed(button, action);
+    }
+
+    private void longPressed(HardwareButton button, Runnable action) {
+        longPressed(button, action, (long) 300.0);
+    }
+
+    private void longPressed(HardwareButton button, Runnable action,long timeout) {
+        mHost.scheduleTask(() -> {
+            if (button.isPressed().get()) {
+                action.run();
+                longPressed(button, action, (long) 100.0);
+            }
+        }, timeout);
+    }
+    
     private void initScenes() {
         mSceneBank = mTrackBank.sceneBank();
         mSceneBank.setIndication(true);
@@ -420,13 +456,14 @@ public class LaunchpadX extends ControllerExtension {
                 slot.isRecordingQueued().markInterested();
                 slot.isStopQueued().markInterested();
                 slot.hasContent().markInterested();
+                slot.color().markInterested();
                 mSessionLayer.bindPressed(mButtons[i][j], () -> {
                     slot.launch();
                 });
 
                 mSessionLayer.bindLightState(() -> {
                     if (slot.isRecording().getAsBoolean())
-                        return RGBState.RED_PULS;
+                        return new RGBState(120, 2); //TODO 
                     if (slot.isPlaying().getAsBoolean())
                         return RGBState.GREEN_PULS;
                     else if (slot.isPlaybackQueued().getAsBoolean())
@@ -436,7 +473,7 @@ public class LaunchpadX extends ControllerExtension {
                     else if (slot.isStopQueued().getAsBoolean())
                         return RGBState.YELLOW;
                     else if (slot.hasContent().get())
-                        return new RGBState(track.color().get());
+                        return new RGBState(slot.color().get());
                     else if (track.arm().getAsBoolean())
                         return RGBState.TRACK_ARM;
                     else
@@ -450,6 +487,7 @@ public class LaunchpadX extends ControllerExtension {
         final HardwareButton button = mHardwareSurface.createHardwareButton(name);
         button.pressedAction().setActionMatcher(mMidiIn0.createCCActionMatcher(0, midi, 127));
         button.releasedAction().setActionMatcher(mMidiIn0.createCCActionMatcher(0, midi, 0));
+        button.isPressed().markInterested();
 
         return button;
     };
@@ -458,6 +496,7 @@ public class LaunchpadX extends ControllerExtension {
         final HardwareButton button = mHardwareSurface.createHardwareButton(name);
         button.pressedAction().setActionMatcher(mMidiIn0.createNoteOnActionMatcher(0, midi));
         button.releasedAction().setActionMatcher(mMidiIn0.createNoteOffActionMatcher(0, midi));
+        button.isPressed().markInterested();
 
         return button;
     };
@@ -513,12 +552,6 @@ public class LaunchpadX extends ControllerExtension {
     private void initButtonLayer(Layer l, int p, RGBState rOn, RGBState rOff) {
         for (int i = 0; i < 8; i++) {
             int ix = i;
-            // clear Pad Matrix
-            // for (int j = 0; j < 8; j++) {
-            // int jx = j;
-            // l.bindLightState(() -> RGBState.OFF, mPadLights[ix][jx]);
-            // l.bindPressed(mButtons[ix][jx], () -> {});
-            // }
             Track t = mTrackBank.getItemAt(ix);
             HardwareActionBindable[] parameter = new HardwareActionBindable[4];
             parameter[0] = t.stopAction();
@@ -532,7 +565,10 @@ public class LaunchpadX extends ControllerExtension {
                         || (p == 1 && t.mute().get())
                         || (p == 2 && t.solo().get())
                         || (p == 3 && t.arm().get()))
-                    return rOn;
+                    if (p == 0 && t.isQueuedForStop().get())
+                        return RGBState.RED_BLINK;
+                    else
+                        return rOn;
                 else if (t.exists().get())
                     return rOff;
                 else
@@ -588,7 +624,7 @@ public class LaunchpadX extends ControllerExtension {
         midiCallback("flush");
     }
 
-    //
+
     private Boolean isNoteIntputActive = false;
 
     // API Objects
