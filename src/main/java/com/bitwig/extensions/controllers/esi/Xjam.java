27149b12fc8f4b2d32107ac411d2fd8dde99285d
Elias <elias@bitwig.com>
2022-10-13 09:26:07 +0200

    [FLOAT-29873] fix Xjam blocks Aftertouch+Pressure

--- src/main/java/com/bitwig/extensions/controllers/esi/Xjam.java
+++ src/main/java/com/bitwig/extensions/controllers/esi/Xjam.java
@@ -26,8 +26,9 @@ public class Xjam extends ControllerExtension {
 
         mMidiIn = mHost.getMidiInPort(0);
 
-        mNoteInput = mMidiIn.createNoteInput("pad", "90????", "D0??","91????", "D1??","92????", "D2??");
+        mNoteInput = mMidiIn.createNoteInput("pad", "9?????", "D?????");
         mNoteInput.setShouldConsumeEvents(false);
+        // mNoteInput = mMidiIn.createNoteInput("pad", "90????", "D0??","91????", "D1??","92????", "D2??");
 
         mCursorTrack = mHost.createCursorTrack(0, 0);
         mCursorDevice = mCursorTrack.createCursorDevice();
