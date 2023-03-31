589645e25cd4f8111c7271317721bf73b8c3dff2
ericahrens <eric.n.ahrens@gmail.com>
2023-03-31 12:11:29 +0200

    Restore Accidental Changes

--- src/main/java/com/bitwig/extensions/controllers/novation/launchpad_pro/StopClipOverlay.java
+++ src/main/java/com/bitwig/extensions/controllers/novation/launchpad_pro/StopClipOverlay.java
@@ -3,25 +3,27 @@ package com.bitwig.extensions.controllers.novation.launchpad_pro;
 import com.bitwig.extension.controller.api.Track;
 import com.bitwig.extension.controller.api.TrackBank;
 
-final class StopClipOverlay extends Overlay {
-   StopClipOverlay(final LaunchpadProControllerExtension driver) {
+final class StopClipOverlay extends Overlay
+{
+   StopClipOverlay(final LaunchpadProControllerExtension driver)
+   {
       super(driver, "stop-clip");
 
       final TrackBank trackBank = driver.mTrackBank;
-      for (int x = 0; x < 8; ++x) {
+      for (int x = 0; x < 8; ++x)
+      {
          final Track track = trackBank.getItemAt(x);
          final Button button = driver.getPadButton(x, 0);
          bindPressed(button, () -> {
             final boolean useAlt = driver.isShiftOn();
-//            if (useAlt)
-//               track.stopAlt();
-//            else
-//               track.stop();
+            if (useAlt)
+               track.stopAlt();
+            else
+               track.stop();
          });
          bindLightState(() -> {
-            if (track.exists().get()) {
+            if (track.exists().get())
                return !track.isStopped().get() ? LedState.STOP_CLIP_ON : LedState.STOP_CLIP_OFF;
-            }
             return LedState.OFF;
          }, button);
       }
@@ -30,9 +32,11 @@ final class StopClipOverlay extends Overlay {
    }
 
    @Override
-   protected void doActivate() {
+   protected void doActivate()
+   {
       final TrackBank trackBank = mDriver.mTrackBank;
-      for (int i = 0; i < 8; ++i) {
+      for (int i = 0; i < 8; ++i)
+      {
          final Track track = trackBank.getItemAt(i);
          track.subscribe();
          track.isStopped().subscribe();
@@ -41,9 +45,11 @@ final class StopClipOverlay extends Overlay {
    }
 
    @Override
-   protected void doDeactivate() {
+   protected void doDeactivate()
+   {
       final TrackBank trackBank = mDriver.mTrackBank;
-      for (int i = 0; i < 8; ++i) {
+      for (int i = 0; i < 8; ++i)
+      {
          final Track track = trackBank.getItemAt(i);
          track.exists().unsubscribe();
          track.isStopped().unsubscribe();
