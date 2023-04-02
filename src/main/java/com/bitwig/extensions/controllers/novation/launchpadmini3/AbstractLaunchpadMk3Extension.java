167cf316e25ce30ca7f0833590c2c5881f41fca1
ericahrens <eric.n.ahrens@gmail.com>
2023-04-02 18:27:22 +0200

    LPMMK3 Fix go back to Session Mode

--- src/main/java/com/bitwig/extensions/controllers/novation/launchpadmini3/AbstractLaunchpadMk3Extension.java
+++ src/main/java/com/bitwig/extensions/controllers/novation/launchpadmini3/AbstractLaunchpadMk3Extension.java
@@ -1,10 +1,11 @@
 package com.bitwig.extensions.controllers.novation.launchpadmini3;
 
-import com.bitwig.extension.api.util.midi.ShortMidiMessage;
-import com.bitwig.extension.callback.ShortMidiMessageReceivedCallback;
 import com.bitwig.extension.controller.ControllerExtension;
 import com.bitwig.extension.controller.ControllerExtensionDefinition;
-import com.bitwig.extension.controller.api.*;
+import com.bitwig.extension.controller.api.ControllerHost;
+import com.bitwig.extension.controller.api.HardwareSurface;
+import com.bitwig.extension.controller.api.MidiIn;
+import com.bitwig.extension.controller.api.MidiOut;
 import com.bitwig.extensions.controllers.novation.commonsmk3.LaunchpadDeviceConfig;
 import com.bitwig.extensions.controllers.novation.commonsmk3.MidiProcessor;
 import com.bitwig.extensions.controllers.novation.launchpadmini3.layers.*;
@@ -19,112 +20,98 @@ import java.util.concurrent.Executors;
 
 public abstract class AbstractLaunchpadMk3Extension extends ControllerExtension {
 
-    protected HardwareSurface surface;
-    protected MidiOut midiOut;
-    protected SessionLayer sessionLayer;
-    protected OverviewLayer overviewLayer;
-    protected LpMode mode = LpMode.SESSION;
-    protected final LaunchpadDeviceConfig deviceConfig;
-    protected Layer currentLayer;
-    protected MidiProcessor midiProcessor;
-    protected HwElements hwElements;
-    private TrackerRegistration trackerRegistration;
+   protected HardwareSurface surface;
+   protected MidiOut midiOut;
+   protected SessionLayer sessionLayer;
+   protected OverviewLayer overviewLayer;
+   protected LpMode mode = LpMode.SESSION;
+   protected final LaunchpadDeviceConfig deviceConfig;
+   protected Layer currentLayer;
+   protected MidiProcessor midiProcessor;
+   protected HwElements hwElements;
+   private TrackerRegistration trackerRegistration;
 
-    protected AbstractLaunchpadMk3Extension(final ControllerExtensionDefinition definition, final ControllerHost host,
-                                            final LaunchpadDeviceConfig deviceConfig) {
-        super(definition, host);
-        this.deviceConfig = deviceConfig;
-    }
+   protected AbstractLaunchpadMk3Extension(final ControllerExtensionDefinition definition, final ControllerHost host,
+                                           final LaunchpadDeviceConfig deviceConfig) {
+      super(definition, host);
+      this.deviceConfig = deviceConfig;
+   }
 
-    protected Context initContext() {
-        DebugMini.registerHost(getHost());
-        final Context diContext = new Context(this);
-        surface = diContext.getService(HardwareSurface.class);
-        diContext.registerService(LaunchpadDeviceConfig.class, deviceConfig);
-        final ControllerHost host = diContext.getService(ControllerHost.class);
-        initMidi(diContext, host);
-        sessionLayer = diContext.create(SessionLayer.class);
-        overviewLayer = diContext.create(OverviewLayer.class);
-        hwElements = diContext.getService(HwElements.class);
-        diContext.create(NotePlayingLayer.class);
-        createControlLayers(diContext);
-        setUpTracking(diContext);
-        return diContext;
-    }
+   protected Context initContext() {
+      DebugMini.registerHost(getHost());
+      final Context diContext = new Context(this);
+      surface = diContext.getService(HardwareSurface.class);
+      diContext.registerService(LaunchpadDeviceConfig.class, deviceConfig);
+      final ControllerHost host = diContext.getService(ControllerHost.class);
+      initMidi(diContext, host);
+      sessionLayer = diContext.create(SessionLayer.class);
+      overviewLayer = diContext.create(OverviewLayer.class);
+      hwElements = diContext.getService(HwElements.class);
+      diContext.create(NotePlayingLayer.class);
+      createControlLayers(diContext);
+      setUpTracking(diContext);
+      return diContext;
+   }
 
-    private void setUpTracking(final Context diContext) {
-        final ViewTracker tracker = diContext.getViewTracker();
-        final ViewCursorControl viewCursor = diContext.getService(ViewCursorControl.class);
-        trackerRegistration = tracker.registerViewPositionListener(deviceConfig.getDeviceId(), (src, trackIndex) -> {
-            DebugMini.println(" Received Track Change %s %d", src, trackIndex);
-            viewCursor.getTrackBank().scrollPosition().set(trackIndex);
-        });
-        viewCursor.getTrackBank()
-                .scrollPosition()
-                .addValueObserver(trackIndex -> tracker.fireViewChanged(deviceConfig.getDeviceId(), trackIndex));
-    }
+   private void setUpTracking(final Context diContext) {
+      final ViewTracker tracker = diContext.getViewTracker();
+      final ViewCursorControl viewCursor = diContext.getService(ViewCursorControl.class);
+      trackerRegistration = tracker.registerViewPositionListener(deviceConfig.getDeviceId(),
+         (src, trackIndex) -> viewCursor.getTrackBank().scrollPosition().set(trackIndex));
+      viewCursor.getTrackBank()
+         .scrollPosition()
+         .addValueObserver(trackIndex -> tracker.fireViewChanged(deviceConfig.getDeviceId(), trackIndex));
+   }
 
-    private void createControlLayers(final Context diContext) {
-        sessionLayer.registerControlLayer(ControlMode.VOLUME, diContext.create(VolumeSliderLayer.class));
-        sessionLayer.registerControlLayer(ControlMode.PAN, diContext.create(PanSliderLayer.class));
-        sessionLayer.registerControlLayer(ControlMode.SENDS, diContext.create(SendsSliderLayer.class));
-        sessionLayer.registerControlLayer(ControlMode.DEVICE, diContext.create(DeviceSliderLayer.class));
-    }
+   private void createControlLayers(final Context diContext) {
+      sessionLayer.registerControlLayer(ControlMode.VOLUME, diContext.create(VolumeSliderLayer.class));
+      sessionLayer.registerControlLayer(ControlMode.PAN, diContext.create(PanSliderLayer.class));
+      sessionLayer.registerControlLayer(ControlMode.SENDS, diContext.create(SendsSliderLayer.class));
+      sessionLayer.registerControlLayer(ControlMode.DEVICE, diContext.create(DeviceSliderLayer.class));
+   }
 
-    protected void initMidi(final Context diContext, final ControllerHost host) {
-        final MidiIn midiIn = host.getMidiInPort(0);
-        final MidiIn midiIn2 = host.getMidiInPort(1);
-        midiIn.setMidiCallback((ShortMidiMessageReceivedCallback) this::onMidi0);
+   protected void initMidi(final Context diContext, final ControllerHost host) {
+      final MidiIn midiIn = host.getMidiInPort(0);
+      final MidiIn midiIn2 = host.getMidiInPort(1);
 
-        midiIn2.setMidiCallback((ShortMidiMessageReceivedCallback) this::onMidi1);
-        // midiIn2.setSysexCallback(this::handleSysEx2);
-        midiOut = host.getMidiOutPort(0);
-        midiProcessor = new MidiProcessor(host, midiIn, midiOut, deviceConfig);
-        diContext.registerService(MidiProcessor.class, midiProcessor);
-        final NoteInput noteInput = midiIn2.createNoteInput("MIDI", "8?????", "9?????", "A?????", "D?????");
-        //noteInput.setShouldConsumeEvents(false);
-        midiIn.setSysexCallback(this::handleSysEx);
-        midiProcessor.start();
-    }
+      midiOut = host.getMidiOutPort(0);
+      midiProcessor = new MidiProcessor(host, midiIn, midiOut, deviceConfig);
+      diContext.registerService(MidiProcessor.class, midiProcessor);
+      midiIn2.createNoteInput("MIDI", "8?????", "9?????", "A?????", "D?????");
+      midiIn.setSysexCallback(this::handleSysEx);
+      midiProcessor.start();
+   }
 
-    protected abstract void handleSysEx(String sysEx);
+   protected abstract void handleSysEx(String sysEx);
 
-    public void onMidi0(final ShortMidiMessage msg) {
-        DebugMini.println("MIDI %02X %02X %02X", msg.getStatusByte(), msg.getData1(), msg.getData2());
-    }
+   private void shutDownController(final CompletableFuture<Boolean> shutdown) {
+      midiProcessor.enableDawMode(false);
+      try {
+         Thread.sleep(300);
+      } catch (final InterruptedException e) {
+         e.printStackTrace();
+      }
+      shutdown.complete(true);
+   }
 
-    public void onMidi1(final ShortMidiMessage msg) {
-        DebugMini.println("MIDI 2 -> %02X %02X %02X", msg.getStatusByte(), msg.getData1(), msg.getData2());
-    }
+   @Override
+   public void flush() {
+      surface.updateHardware();
+   }
 
-    private void shutDownController(final CompletableFuture<Boolean> shutdown) {
-        midiProcessor.enableDawMode(false);
-        try {
-            Thread.sleep(300);
-        } catch (final InterruptedException e) {
-            e.printStackTrace();
-        }
-        shutdown.complete(true);
-    }
-
-    @Override
-    public void flush() {
-        surface.updateHardware();
-    }
-
-    @Override
-    public void exit() {
-        if (trackerRegistration != null) {
-            trackerRegistration.unregister();
-        }
-        final CompletableFuture<Boolean> shutdown = new CompletableFuture<>();
-        Executors.newSingleThreadExecutor().execute(() -> shutDownController(shutdown));
-        try {
-            shutdown.get();
-        } catch (final InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-    }
+   @Override
+   public void exit() {
+      if (trackerRegistration != null) {
+         trackerRegistration.unregister();
+      }
+      final CompletableFuture<Boolean> shutdown = new CompletableFuture<>();
+      Executors.newSingleThreadExecutor().execute(() -> shutDownController(shutdown));
+      try {
+         shutdown.get();
+      } catch (final InterruptedException | ExecutionException e) {
+         e.printStackTrace();
+      }
+   }
 
 
 }
--- src/main/java/com/bitwig/extensions/controllers/novation/launchpadmini3/LaunchPadXExtensionDefinition.java
+++ src/main/java/com/bitwig/extensions/controllers/novation/launchpadmini3/LaunchPadXExtensionDefinition.java
@@ -9,55 +9,55 @@ import com.bitwig.extension.controller.api.ControllerHost;
 import java.util.UUID;
 
 public class LaunchPadXExtensionDefinition extends ControllerExtensionDefinition {
-    private static final UUID DRIVER_ID = UUID.fromString("6da975db-1967-47d5-8b5f-3ee29e84e0c7");
+   private static final UUID DRIVER_ID = UUID.fromString("6da975db-1967-47d5-8b5f-3ee29e84e0c7");
 
-    public LaunchPadXExtensionDefinition() {
-    }
+   public LaunchPadXExtensionDefinition() {
+   }
 
-    @Override
-    public String getName() {
-        return "Launchpad X";
-    }
+   @Override
+   public String getName() {
+      return "Launchpad X";
+   }
 
-    @Override
-    public String getAuthor() {
-        return "Bitwig";
-    }
+   @Override
+   public String getAuthor() {
+      return "Bitwig";
+   }
 
-    @Override
-    public String getVersion() {
-        return "0.8";
-    }
+   @Override
+   public String getVersion() {
+      return "1.0";
+   }
 
-    @Override
-    public UUID getId() {
-        return DRIVER_ID;
-    }
+   @Override
+   public UUID getId() {
+      return DRIVER_ID;
+   }
 
-    @Override
-    public String getHardwareVendor() {
-        return "Novation";
-    }
+   @Override
+   public String getHardwareVendor() {
+      return "Novation";
+   }
 
-    @Override
-    public String getHardwareModel() {
-        return "Launchpad X";
-    }
+   @Override
+   public String getHardwareModel() {
+      return "Launchpad X";
+   }
 
-    @Override
-    public int getRequiredAPIVersion() {
-        return 18;
-    }
+   @Override
+   public int getRequiredAPIVersion() {
+      return 18;
+   }
 
-    @Override
-    public int getNumMidiInPorts() {
-        return 2;
-    }
+   @Override
+   public int getNumMidiInPorts() {
+      return 2;
+   }
 
-    @Override
-    public int getNumMidiOutPorts() {
-        return 1;
-    }
+   @Override
+   public int getNumMidiOutPorts() {
+      return 1;
+   }
 
 
 //    Added MIDI IN: LPX MIDI
@@ -65,31 +65,31 @@ public class LaunchPadXExtensionDefinition extends ControllerExtensionDefinition
 //    Added MIDI OUT: LPX MIDI
 //    Added MIDI OUT: MIDIOUT2 (LPX MIDI)
 
-    @Override
-    public void listAutoDetectionMidiPortNames(final AutoDetectionMidiPortNamesList list,
-                                               final PlatformType platformType) {
-        final String[] inputNames = new String[2];
-        final String[] outputNames = new String[1];
+   @Override
+   public void listAutoDetectionMidiPortNames(final AutoDetectionMidiPortNamesList list,
+                                              final PlatformType platformType) {
+      final String[] inputNames = new String[2];
+      final String[] outputNames = new String[1];
 
-        switch (platformType) {
-            case LINUX:
-            case WINDOWS:
-                inputNames[0] = "LPX MIDI";
-                inputNames[1] = "MIDIIN2 (LPX MIDI)";
-                outputNames[0] = "LPX MIDI";
-                break;
-            case MAC:
-                inputNames[0] = "Launchpad X MK3 LPX DAW Out";
-                inputNames[1] = "Launchpad X MK3 LPX MIDI Out";
-                outputNames[0] = "Launchpad X MK3 LPX DAW In";
-                break;
-        }
+      switch (platformType) {
+         case LINUX:
+         case WINDOWS:
+            inputNames[0] = "LPX MIDI";
+            inputNames[1] = "MIDIIN2 (LPX MIDI)";
+            outputNames[0] = "LPX MIDI";
+            break;
+         case MAC:
+            inputNames[0] = "Launchpad X MK3 LPX DAW Out";
+            inputNames[1] = "Launchpad X MK3 LPX MIDI Out";
+            outputNames[0] = "Launchpad X MK3 LPX DAW In";
+            break;
+      }
 
-        list.add(inputNames, outputNames);
-    }
+      list.add(inputNames, outputNames);
+   }
 
-    @Override
-    public ControllerExtension createInstance(final ControllerHost host) {
-        return new LaunchpadXControllerExtension(this, host);
-    }
+   @Override
+   public ControllerExtension createInstance(final ControllerHost host) {
+      return new LaunchpadXControllerExtension(this, host);
+   }
 }
