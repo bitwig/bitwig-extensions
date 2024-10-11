766aad464deac7807bbea9cb9da5bc2ee32b8890
ericahrens <eric.n.ahrens@gmail.com>
2024-10-11 21:58:07 +0200

    LaunchkeyMk4 Mac Ports

--- src/main/java/com/bitwig/extensions/controllers/novation/launchkey_mk4/DrumPadLayer.java
+++ src/main/java/com/bitwig/extensions/controllers/novation/launchkey_mk4/DrumPadLayer.java
@@ -1,4 +1,15 @@
 package com.bitwig.extensions.controllers.novation.launchkey_mk4;
 
-public class DrumPadLayer {
+import com.bitwig.extensions.controllers.arturia.keylab.mk3.MidiProcessor;
+import com.bitwig.extensions.framework.Layer;
+import com.bitwig.extensions.framework.Layers;
+import com.bitwig.extensions.framework.di.Component;
+
+@Component
+public class DrumPadLayer extends Layer {
+   private final Integer[] noteTable = new Integer[128];
+
+   public DrumPadLayer(Layers layers, LaunchkeyHwElements hwElements, MidiProcessor midiProcessor) {
+      super(layers, "DRUM_PAD");
+   }
 }
